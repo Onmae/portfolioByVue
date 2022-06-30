9f5551b2c65a64cf173ae9daa388da370073e0d3
Onmae <37141155+Onmae@users.noreply.github.com>
2022-06-30 18:56:58 +0900

    init

---
+++ server/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  ignorePatterns: ['!.eslintrc.js'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': 'off',
+    'import/order': 'off',
+    'no-use-before-define': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+};
